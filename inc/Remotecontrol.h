a1bbd043fa76776e4e046ab192c799bbe641a863
hanghang177 <weihang@illinois.edu>
2018-02-18 20:57:19 -0600

    Updated file struct

---
+++ inc/Remotecontrol.h
@@ -0,0 +1,17 @@
+#define joystickthreshold 15
+#define autogoaldisttime 5000
+#define turn90time 500
+
+void intake();
+void outthrow();
+void raisearm();
+void putdownarm();
+void takecone();
+void releasecone();
+void goforward();
+void turn90(bool right);
+void filterRT();
+void mecanumdrive();
+void skiddrive();
+void go();
+void debugprint();
